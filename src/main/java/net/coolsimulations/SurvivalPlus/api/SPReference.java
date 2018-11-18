45190f761100d31888980a30dc24852fc901250f
coolsim <coolsimulations@gmail.com>
2018-11-19 08:33:02 +1000

    Fixed Mod Name

--- src/main/java/net/coolsimulations/SurvivalPlus/api/SPReference.java
+++ src/main/java/net/coolsimulations/SurvivalPlus/api/SPReference.java
@@ -2,7 +2,7 @@ package net.coolsimulations.SurvivalPlus.api;
 
 public class SPReference {
 	public static final String MOD_ID = "survivalplus";
-	public static final String MOD_NAME = "Survival Plus";
+	public static final String MOD_NAME = "SurvivalPlus";
 	public static final String VERSION = "0.1.0";
 	public static final String ACCEPTED_VERSIONS = "[1.10, 1.10.2]";
 	public static final String CLIENT_PROXY_CLASS = "net.coolsimulations.SurvivalPlus.core.proxy.ClientProxy";
