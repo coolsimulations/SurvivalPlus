603e2cc02b31f4053ed5b9db87752f79144ad762
coolsim <coolsimulations@gmail.com>
2021-08-01 20:47:50 +1000

    Re-Added Pearl Block

--- src/main/java/net/coolsimulations/SurvivalPlus/primer/init/SurvivalPlusPrimerBlocks.java
+++ src/main/java/net/coolsimulations/SurvivalPlus/primer/init/SurvivalPlusPrimerBlocks.java
@@ -17,6 +17,7 @@ public class SurvivalPlusPrimerBlocks {
 	public static Block copper_ore;
 	public static Block amethyst;
 	public static Block ruby;
+	public static Block pearl;
 	public static Block topaz;
 	public static Block sapphire;
 	public static Block spinel;
@@ -27,6 +28,7 @@ public class SurvivalPlusPrimerBlocks {
 		copper_ore = new SPBlockOre(SPBlockOre.Resource.TIER_1, false).setRegistryName(SPReference.MOD_ID, "copper_ore");
 		amethyst = new SPBlockCrystal(MaterialColor.COLOR_PURPLE).setRegistryName(SPReference.MOD_ID, "amethyst");
 		ruby = new SPBlockCrystal(MaterialColor.COLOR_RED).setRegistryName(SPReference.MOD_ID, "ruby");
+		pearl = new SPBlockCrystal(MaterialColor.SNOW).setRegistryName(SPReference.MOD_ID, "pearl");
 		topaz = new SPBlockCrystal(MaterialColor.COLOR_YELLOW).setRegistryName(SPReference.MOD_ID, "topaz");
 		sapphire = new SPBlockCrystal(MaterialColor.COLOR_BLUE).setRegistryName(SPReference.MOD_ID, "sapphire");
 		spinel = new SPBlockCrystal(MaterialColor.COLOR_BLACK).setRegistryName(SPReference.MOD_ID, "spinel");
@@ -39,13 +41,18 @@ public class SurvivalPlusPrimerBlocks {
 		registerBlock(copper_ore);
 		registerBlock(amethyst);
 		registerBlock(ruby);
+		registerBlock(pearl, false);
 		registerBlock(topaz);
 		registerBlock(sapphire);
 		registerBlock(spinel);
 
 	}
-
+	
 	public static void registerBlock(Block block) {
+		registerBlock(block, true);
+	}
+
+	public static void registerBlock(Block block, boolean registerItem) {
 
 		BlockItem blockItem;
 
@@ -57,7 +64,8 @@ public class SurvivalPlusPrimerBlocks {
 
 		blockItem.setRegistryName(block.getRegistryName());
 		ForgeRegistries.BLOCKS.register(block);
-		ForgeRegistries.ITEMS.register(blockItem);
+		if(registerItem)
+			ForgeRegistries.ITEMS.register(blockItem);
 	}
 
 }
