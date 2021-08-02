f9ca913e2b5f31428039f5cc683ff1202e49d4e3
coolsim <coolsimulations@gmail.com>
2021-08-02 16:32:51 +1000

    Fixed No Tree Punching Support

--- src/main/java/net/coolsimulations/SurvivalPlus/core/util/SurvivalPlusNoTreePunching.java
+++ src/main/java/net/coolsimulations/SurvivalPlus/core/util/SurvivalPlusNoTreePunching.java
@@ -1,13 +1,13 @@
 package net.coolsimulations.SurvivalPlus.core.util;
 
 import com.alcatrazescapee.notreepunching.common.ModItemGroup;
-import com.alcatrazescapee.notreepunching.common.items.CraftingAxeItem;
 import com.alcatrazescapee.notreepunching.common.items.KnifeItem;
 import com.alcatrazescapee.notreepunching.common.items.MattockItem;
 
 import net.coolsimulations.SurvivalPlus.api.SPItems;
 import net.coolsimulations.SurvivalPlus.api.item.SPItemTier;
 import net.coolsimulations.SurvivalPlus.core.init.SurvivalPlusTools;
+import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.Item.Properties;
 
 public class SurvivalPlusNoTreePunching {
@@ -15,7 +15,7 @@ public class SurvivalPlusNoTreePunching {
 	public static void init() {
 		SPItems.titanium_knife = new KnifeItem(SPItemTier.titaniumToolMaterial, 1, -2.2F, new Properties().tab(ModItemGroup.ITEMS)).setRegistryName("titanium_knife");
 		SPItems.titanium_mattock = new MattockItem(SPItemTier.titaniumToolMaterial, 0.5F, -2.2F, new Properties().tab(ModItemGroup.ITEMS)).setRegistryName("titanium_mattock");
-		SPItems.titanium_saw = new CraftingAxeItem(SPItemTier.titaniumToolMaterial, 3.0F, -3.2F, new Properties().tab(ModItemGroup.ITEMS)).setRegistryName("titanium_saw");
+		SPItems.titanium_saw = new AxeItem(SPItemTier.titaniumToolMaterial, 3.0F, -3.2F, new Properties().tab(ModItemGroup.ITEMS)).setRegistryName("titanium_saw");
 		
 		/**if(SPCompatibilityManager.isGCLoaded() && SPCompatibilityManager.isGCPLoaded()) {
 			SPItems.desh_knife = new ItemKnife(MarsItems.TOOLDESH).setUnlocalizedName("desh_knife").setRegistryName("desh_knife").setCreativeTab(ModTabs.TAB_TOOLS);
