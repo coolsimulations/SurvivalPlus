54f635e68455025264829fa912f4ca39d7c8f57f
coolsimulations <coolsimulations@gmail.com>
2021-12-08 12:07:40 +1000

    Removed BOP Ethereal Wooden Armor

--- src/main/java/net/coolsimulations/SurvivalPlus/core/init/FuelHandler.java
+++ src/main/java/net/coolsimulations/SurvivalPlus/core/init/FuelHandler.java
@@ -38,7 +38,6 @@ public class FuelHandler{
 
 			addArmorFuels(SPItems.cherry);
 			addArmorFuels(SPItems.dead);
-			addArmorFuels(SPItems.ethereal);
 			addArmorFuels(SPItems.fir);
 			addArmorFuels(SPItems.hellbark);
 			addArmorFuels(SPItems.jacaranda);
@@ -57,6 +56,7 @@ public class FuelHandler{
 			addArmorFuels(SPItems.mangrove);
 			addArmorFuels(SPItems.pine);
 			addArmorFuels(SPItems.sacred_oak);
+			addArmorFuels(SPItems.ethereal);
 		}
 
 		if(SPCompatibilityManager.isForestryLoaded()) {
