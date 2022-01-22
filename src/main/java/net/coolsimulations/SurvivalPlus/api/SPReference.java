023a84463ee3df613d25356c55c7dc046b387f9f
coolsimulations <coolsimulations@gmail.com>
2022-01-22 14:58:23 +1000

    Version 0.1.6b

--- src/main/java/net/coolsimulations/SurvivalPlus/api/SPReference.java
+++ src/main/java/net/coolsimulations/SurvivalPlus/api/SPReference.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class SPReference {
 	public static final String MOD_ID = "survivalplus";
 	public static final String MOD_NAME = "SurvivalPlus";
-	public static final String VERSION = "0.1.6";
+	public static final String VERSION = "0.1.6b";
 	public static final String ACCEPTED_VERSIONS = "[1.17, 1.17.1]";
 	public static final String CLIENT_PROXY_CLASS = "net.coolsimulations.SurvivalPlus.core.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "net.coolsimulations.SurvivalPlus.core.proxy.ServerProxy";
