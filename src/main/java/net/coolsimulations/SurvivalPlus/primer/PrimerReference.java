51219e404f170e5d64deaace90ceec9dc2cd62fa
coolsim <coolsimulations@gmail.com>
2022-06-10 11:33:24 +1000

    Update to 1.19

--- src/main/java/net/coolsimulations/SurvivalPlus/primer/PrimerReference.java
+++ src/main/java/net/coolsimulations/SurvivalPlus/primer/PrimerReference.java
@@ -4,6 +4,6 @@ public class PrimerReference {
 	
 	public static final String MOD_ID = "survivalplus_primer";
 	public static final String MOD_NAME = "SurvivalPlus Primer";
-	public static final String VERSION = "0.1.0c";
+	public static final String VERSION = "0.1.0d";
 
 }
